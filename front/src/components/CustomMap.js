68a054db4c594bfb55e5f78610d5244fddef3242
lfgaray0331 <74473239+lfgaray0331@users.noreply.github.com>
2021-09-22 23:19:05 -0500

    CustomMap.js
    Implementación polyline

--- front/src/components/CustomMap.js
+++ front/src/components/CustomMap.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup , Polyline} from "react-leaflet";
 import L from "leaflet";
 
 const useStyles = makeStyles((theme) => ({
@@ -11,11 +11,18 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const center = [10.9877224, -74.788559, 3];
-
+const coords=[]
+var last_element={}
 const icon = L.icon({ iconUrl: "/images/marker-icon.png" });
 
 function CustomMap({ data }) {
   const classes = useStyles();
+  var position = [data[0], data[1]];
+  coords.push(position);
+  last_element = coords.slice();
+  // delete last_element1[0];
+  // delete last_element1[1];
+  
   return (
     <MapContainer
       style={{ width: "100%", height: "100vh" }}
@@ -34,6 +41,10 @@ function CustomMap({ data }) {
           <p>Longitud:{data[1]}</p>
         </Popup>
       </Marker>
+      <Polyline
+        positions={[[last_element]]}
+        color={'black'}
+      />
     </MapContainer>
   );
 }
