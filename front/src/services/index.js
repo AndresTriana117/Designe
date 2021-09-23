98c1817e5708e3e95016d79c7ac3977283a4228a
briantuninorte <89306508+briantuninorte@users.noreply.github.com>
2021-09-22 19:10:48 -0500

    Update index.js

--- front/src/services/index.js
+++ front/src/services/index.js
@@ -4,12 +4,14 @@ export const consulta = async (url) => {
   let num_latitud = 10.9877224;
   let num_longitud = -74.7885593;
   let fecha = 2021;
+  let hora = 2000
 
   try {
     const res = await axios.get(url);
     num_latitud = res.data.response.latitude;
     num_longitud = res.data.response.longitude;
     fecha = res.data.response.fecha;
+    hora = res.data.response.hora;
   } catch (error) {
     console.log(error);
   }
@@ -18,5 +20,6 @@ export const consulta = async (url) => {
     lat: num_latitud,
     lng: num_longitud,
     fecha,
+    hora,
   };
 };
